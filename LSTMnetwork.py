10f5aa8c1bb9e2373146e65099e73ba5d46faa25
Sentientplatypus <trexxxxxxxxxxy@gmail.com>
2022-08-14 22:20:09 -0400

    changed predictions for future

--- LSTMnetwork.py
+++ LSTMnetwork.py
@@ -1,5 +1,25 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-dataset = pd.read_csv('airline-passengers.csv', usecols=[1], engine='python')
-plt.plot(dataset)
+import tensorflow as tf
+from keras import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+import numpy as np
+
+
+dataframe:pd.DataFrame = pd.read_csv('airline-passengers.csv', usecols=[1], engine='python')
+plt.plot(dataframe)
 plt.show()
+
+tf.random.set_seed(7)
+
+dataset:np.ndarray = dataframe.values
+lengthOfPredictions = len(dataset) * 0.5
+dataset = np.append(dataset, np.repeat(np.NaN, lengthOfPredictions))
+dataset = dataset.astype('float32')
+print(dataset)
+
+scaler = MinMaxScaler(feature_range=(0,1))
+dataset = scaler.fit_transform(dataset)

16b1e95f4918be55328a6c6360e55533aedd94d4
Sentientplatypus <trexxxxxxxxxxy@gmail.com>
2022-08-16 22:11:32 -0400

    found new script

--- LSTMnetwork.py
+++ LSTMnetwork.py
@@ -1,25 +1,72 @@
+import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+import yfinance as yf
 import tensorflow as tf
 from keras import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import mean_squared_error
-import numpy as np
+import matplotlib.pyplot as plt
+pd.options.mode.chained_assignment = None
+tf.random.set_seed(0)
+print("we out")
 
+# download the data
+df = yf.download(tickers=['TSLA'], period='3m')
+y = df['Close'].fillna(method='ffill')
+y = y.values.reshape(-1, 1)
 
-dataframe:pd.DataFrame = pd.read_csv('airline-passengers.csv', usecols=[1], engine='python')
-plt.plot(dataframe)
+# scale the data
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaler = scaler.fit(y)
+y = scaler.transform(y)
+
+# generate the input and output sequences
+n_lookback = 60  # length of input sequences (lookback period)
+n_forecast = 30  # length of output sequences (forecast period)
+
+X = []
+Y = []
+
+for i in range(n_lookback, len(y) - n_forecast + 1):
+    X.append(y[i - n_lookback: i])
+    Y.append(y[i: i + n_forecast])
+
+X = np.array(X)
+Y = np.array(Y)
+
+# fit the model
+model = Sequential()
+model.add(LSTM(units=50, return_sequences=True, input_shape=(n_lookback, 1)))
+model.add(LSTM(units=50))
+model.add(Dense(n_forecast))
+
+model.compile(loss='mean_squared_error', optimizer='adam')
+model.fit(X, Y, epochs=100, batch_size=32, verbose=0)
+
+# generate the forecasts
+X_ = y[- n_lookback:]  # last available input sequence
+X_ = X_.reshape(1, n_lookback, 1)
+
+Y_ = model.predict(X_).reshape(-1, 1)
+Y_ = scaler.inverse_transform(Y_)
+
+# organize the results in a data frame
+df_past = df[['Close']].reset_index()
+df_past.rename(columns={'index': 'Date', 'Close': 'Actual'}, inplace=True)
+df_past['Date'] = pd.to_datetime(df_past['Date'])
+df_past['Forecast'] = np.nan
+df_past['Forecast'].iloc[-1] = df_past['Actual'].iloc[-1]
+
+df_future = pd.DataFrame(columns=['Date', 'Actual', 'Forecast'])
+df_future['Date'] = pd.date_range(start=df_past['Date'].iloc[-1] + pd.Timedelta(days=1), periods=n_forecast)
+df_future['Forecast'] = Y_.flatten()
+df_future['Actual'] = np.nan
+
+results = df_past.append(df_future).set_index('Date')
+
+# plot the results
+results.plot(title='AAPL')
 plt.show()
-
-tf.random.set_seed(7)
-
-dataset:np.ndarray = dataframe.values
-lengthOfPredictions = len(dataset) * 0.5
-dataset = np.append(dataset, np.repeat(np.NaN, lengthOfPredictions))
-dataset = dataset.astype('float32')
-print(dataset)
-
-scaler = MinMaxScaler(feature_range=(0,1))
-dataset = scaler.fit_transform(dataset)
+print("we done")
+print(results.columns)
